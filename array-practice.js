1c0e1f76c1040302c9cc5509b463cfebd21e7269
royce <royce20002006@yahoo.com>
2024-01-27 03:26:59 -0600

    finished 3 questions

--- array-practice.js
+++ array-practice.js
@@ -1,22 +1,41 @@
 const findMinimum = arr => {
-
-  // Your code here 
-
+  let smallest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) smallest = arr[i];
+    
+  }
+  return smallest;
+  
+  
 };
 
 const runningSum = arr => {
+  
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i - 1] + arr[i];
+  }
+  return arr;
 
   // Your code here 
 };
 
 const evenNumOfChars = arr => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+    if (str.length % 2 === 0) count++;
+  }
+  return count;
 
   // Your code here 
 };
 
 const smallerThanCurr = arr => {
-
-  // Your code here 
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    
+  }
+ 
 
 };
 
