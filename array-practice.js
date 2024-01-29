2c8bc2abcf05bfc18aea5dd47207fffc388bdb17
royce <royce20002006@yahoo.com>
2024-01-28 19:03:28 -0600

    finished two sum

--- array-practice.js
+++ array-practice.js
@@ -46,7 +46,12 @@ const smallerThanCurr = arr => {
 
 
 const twoSum = (arr, target) => {
- 
+ for (let i  = 0; i < arr.length; i++) {
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] + arr[j] === target) return true;
+  }
+ }
+ return false;
  
 };
 
